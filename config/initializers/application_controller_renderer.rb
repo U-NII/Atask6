420c5ee2aa7be893970086ab9dd45aa90e9797be
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-04-25 08:27:55 +0000

    [Start]課題3提出

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '77b7a39ab4b6ec3126d59e8cacdeed25703002b71ab8c92c7957b74ba82ab3498894f643adb77366a6292462aa960842b9f55b90031e68fe9df6383bbe62b8b0'
