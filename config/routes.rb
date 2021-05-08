6af5d461e0a417261596fd19ac7a53efb2e273ab
U-NII <sinnrabannsyou.0715.yoro@gmail.com>
2021-05-08 04:44:49 +0000

    [Finish]応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "home/about"=>"homes#about"
+  get '/search', to: 'searchs#search'
   resources :users
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
+  resources :searchs
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
   resources :users
